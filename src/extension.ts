d5a2a7a1ddb91048347e768d9d2ee38d8abd2771
fabrisdev <ezepuntocom@hotmail.com>
2026-05-10 22:51:18 -0300

    Simplify code

--- src/extension.ts
+++ src/extension.ts
@@ -15,28 +15,59 @@ export function activate(context: vscode.ExtensionContext) {
 					.split("\n")
 					.map((line) => line.trim());
 				const currentLine = lines[position.line];
-				const result = /<JMN ([0-9]+)>/.exec(currentLine);
-				if (result === null) return;
-				const [_, id] = result;
-				const definitionToSearch = `<LBN ${id}>`;
-				const definitionLine = lines.indexOf(definitionToSearch);
-				if (definitionLine === -1) {
-					vscode.window.showErrorMessage(`LBN id ${id} not found.`);
-					return;
-				}
-				const definitionLength = definitionToSearch.length;
 
-				const start = new vscode.Position(definitionLine, 0);
-				const end = new vscode.Position(definitionLine, definitionLength);
+				const jumpFromTo = {
+					JMN: "LBN",
+					JMP: "LAB",
+				};
+				const range = Object.entries(jumpFromTo)
+					.map(([jmpLabel, lbnLabel]) =>
+						jumpToDefinition({ lines, currentLine, jmpLabel, lbnLabel }),
+					)
+					.filter((range) => range !== undefined)
+					.at(0);
+				if (!range) return undefined;
 				return {
 					uri: document.uri,
-					range: new vscode.Range(start, end),
+					range,
 				};
 			},
 		},
 	);
 
 	context.subscriptions.push(definitionDisposable);
+}
+
+interface FileInfo {
+	lines: string[];
+	currentLine: string;
+}
+
+interface JumpToDefiniton extends FileInfo {
+	jmpLabel: string;
+	lbnLabel: string;
+}
+
+function jumpToDefinition({
+	lines,
+	currentLine,
+	jmpLabel,
+	lbnLabel,
+}: JumpToDefiniton) {
+	const result = new RegExp(`<${jmpLabel} ([a-zA-Z0-9_]+)>`).exec(currentLine);
+	if (result === null) return;
+	const [_, id] = result;
+	const definitionToSearch = `<${lbnLabel} ${id}>`;
+	const definitionLine = lines.indexOf(definitionToSearch);
+	if (definitionLine === -1) {
+		vscode.window.showErrorMessage(`${lbnLabel} id ${id} not found.`);
+		return;
+	}
+	const definitionLength = definitionToSearch.length;
+
+	const start = new vscode.Position(definitionLine, 0);
+	const end = new vscode.Position(definitionLine, definitionLength);
+	return new vscode.Range(start, end);
 }
 
 export function deactivate() {}
